3090dc2b78280e2cfcd9771d6c8b399c5fc32bfb
Tim Lupfer <tim@sundaysenergy.com>
2013-11-25 18:46:47 -0600

    Update assets

---
+++ update_assets.js
@@ -0,0 +1,50 @@
+var https = require('https');
+
+var options = {
+  hostname: process.env.CLOUDANT_URL,
+  port: 443,
+  path: process.env.CLOUDANT_PATH,
+  method: 'HEAD',
+  auth: process.env.CLOUDANT_AUTH
+};
+
+function pushedFile(element) {
+  return element.match(/(\.css|\.js)/);
+}
+
+var req = https.request(options, function(res) {
+  var rev = res.headers.etag;
+  var path = process.env.CLOUDANT_PATH;
+  if (res.statusCode != 404) {
+    path = path + "?rev=" + rev.replace(/\"/g,'');
+  }
+  res.on('data', function(d) {
+    process.stdout.write(d);
+  });
+  var files = require('findit2').sync(__dirname);
+  var update = {};
+  update.files = files.filter(pushedFile);
+  for (i in update.files) {
+    update.files[i] = update.files[i].replace(__dirname, '');
+  }
+  update.container = process.env.CLOUDFILES_CONTAINER;
+  var updateoptions = {
+    hostname: process.env.CLOUDANT_URL,
+    port: 443,
+    path: path,
+    method: 'PUT',
+    headers: { 
+                'Content-Type':'application/json', 
+                'Content-Length':JSON.stringify(update).length 
+             },
+    auth: process.env.CLOUDANT_AUTH
+  }
+  var updatereq = https.request(updateoptions, function(res) {
+    res.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+    });
+  });
+  updatereq.write(JSON.stringify(update));
+  updatereq.end();
+});
+req.end();
