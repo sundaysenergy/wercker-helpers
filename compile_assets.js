ff9ecdaa0c6dfdb234faf21ee657e5040b2154a8
Tim Lupfer <tim@sundaysenergy.com>
2013-11-27 16:11:02 -0600

    Compile assets programmatically

--- compile_assets.js
+++ compile_assets.js
@@ -1,8 +1,25 @@
-require('js-yaml');
+var jsyaml = require('js-yaml');
+var less = require('less');
+var fs = require('fs');
 
-try {
-  var doc = require(process.env.WERCKER_ROOT + '/collection.yml');
-  console.log(doc);
-} catch (e) {
-  console.log(e);
+fs.mkdir(process.env.WERCKER_GIT_COMMIT);
+var doc = require(process.env.WERCKER_ROOT + '/collection.yml');
+var defaultcss = [];
+for (k in doc.default.css) {
+  defaultcss.push(fs.readFileSync(process.env.WERCKER_ROOT + '/' + doc.default.css[k]).toString());
+}
+for (i in doc.pages) {
+  var parser = new(less.Parser)({
+    paths: [process.env.WERCKER_ROOT]
+  });
+  var files = [];
+  for (j in doc.pages[i].css) {
+    files.push(fs.readFileSync(process.env.WERCKER_ROOT + '/' + doc.pages[i].css[j]).toString());
+  }
+  parser.parse(defaultcss.join(" ") + files.join(" "), function (e, tree) {
+    var css = tree.toCSS({ compress: true });
+    fs.writeFile(process.env.WERCKER_ROOT + '/' + process.env.WERCKER_GIT_COMMIT + '/' + i + '.css', css, function (err) {
+      if (err) throw err;
+    });
+  });
 }
