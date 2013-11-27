a5aa18eea8c8ade7d89899e63c38755c7b9656b9
Tim Lupfer <tim@sundaysenergy.com>
2013-11-27 17:07:13 -0600

    Trap current_page variable

--- compile_assets.js
+++ compile_assets.js
@@ -8,18 +8,19 @@ var defaultcss = [];
 for (k in doc.default.css) {
   defaultcss.push(fs.readFileSync(process.env.WERCKER_ROOT + '/' + doc.default.css[k]).toString());
 }
-for (i in doc.pages) {
-  var parser = new(less.Parser)({
-    paths: [process.env.WERCKER_ROOT]
-  });
-  var files = [];
-  for (j in doc.pages[i].css) {
-    files.push(fs.readFileSync(process.env.WERCKER_ROOT + '/' + doc.pages[i].css[j]).toString());
-  }
-  parser.parse(defaultcss.join(" ") + files.join(" "), function (e, tree) {
-    var css = tree.toCSS({ compress: true });
-    fs.writeFile(process.env.WERCKER_ROOT + '/' + process.env.WERCKER_GIT_COMMIT + '/' + i + '.css', css, function (err) {
-      if (err) throw err;
+for (var i in doc.pages) {
+  (function(current_page) {
+    var parser = new(less.Parser)({
+      paths: [process.env.WERCKER_ROOT]
     });
-  });
+    var files = [];
+    for (j in doc.pages[current_page].css) {
+      files.push(fs.readFileSync(process.env.WERCKER_ROOT + '/' + doc.pages[current_page].css[j]).toString());
+    }
+    console.log(current_page);
+    parser.parse(defaultcss.join(" ") + files.join(" "), function (e, tree) {
+      var css = tree.toCSS({ compress: true });
+      fs.writeFileSync(process.env.WERCKER_ROOT + '/' + process.env.WERCKER_GIT_COMMIT + '/' + current_page + '.css', css);
+    });
+  })(i);
 }
