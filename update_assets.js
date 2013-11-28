0d7130e6f9ca9a7a8e8c1beb6fd41d1f690becbe
Tim Lupfer <tim@sundaysenergy.com>
2013-11-27 21:14:26 -0600

    More typos

--- update_assets.js
+++ update_assets.js
@@ -34,10 +34,10 @@ var req = https.request(options, function(res) {
       update.collection[key] = {};
       update.collection[key].js = '';
       update.collection[key].css = '';
-      if (fs.existsSync(process.env.WERCKER_ROOT + '/' + process.env.WERCKER_GIT_COMMIT + '/' + key + '.js') {
+      if (fs.existsSync(process.env.WERCKER_ROOT + '/' + process.env.WERCKER_GIT_COMMIT + '/' + key + '.js')) {
         update.collection[key].js = 'http://' + process.env.CLOUDFILES_CONTAINER + '/' + process.env.WERCKER_GIT_COMMIT + '/' + key + '.js';
       }
-      if (fs.existsSync(process.env.WERCKER_ROOT + '/' + process.env.WERCKER_GIT_COMMIT + '/' + key + '.css') {
+      if (fs.existsSync(process.env.WERCKER_ROOT + '/' + process.env.WERCKER_GIT_COMMIT + '/' + key + '.css')) {
         update.collection[key].js = 'http://' + process.env.CLOUDFILES_CONTAINER + '/' + process.env.WERCKER_GIT_COMMIT + '/' + key + '.css';
       }
     })(i);
