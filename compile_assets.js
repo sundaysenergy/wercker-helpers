af926f9385a3c8e965688f96e0e7447ef0ae1c41
Tim Lupfer <tim@sundaysenergy.com>
2013-11-27 20:14:36 -0600

    process default like any other collection key

--- compile_assets.js
+++ compile_assets.js
@@ -2,23 +2,25 @@ var jsyaml = require('js-yaml');
 var less = require('less');
 var fs = require('fs');
 
+// Make a directory based on the commit hash
 fs.mkdir(process.env.WERCKER_GIT_COMMIT);
+
+// Process the yaml file telling us about our collections.
 var doc = require(process.env.WERCKER_ROOT + '/collection.yml');
-var defaultcss = [];
-for (k in doc.default.css) {
-  defaultcss.push(fs.readFileSync(process.env.WERCKER_ROOT + '/' + doc.default.css[k]).toString());
-}
-for (var i in doc.pages) {
+
+// Process each key
+for (var i in doc) {
   (function(current_page) {
     var parser = new(less.Parser)({
       paths: [process.env.WERCKER_ROOT]
     });
     var files = [];
-    for (j in doc.pages[current_page].css) {
-      files.push(fs.readFileSync(process.env.WERCKER_ROOT + '/' + doc.pages[current_page].css[j]).toString());
+    // Push each file into an array as a string
+    for (j in doc[current_page].css) {
+      files.push(fs.readFileSync(process.env.WERCKER_ROOT + '/' + doc[current_page].css[j]).toString());
     }
-    console.log(current_page);
-    parser.parse(defaultcss.join(" ") + files.join(" "), function (e, tree) {
+    // Parse the less and write a css file
+    parser.parse(files.join(" "), function (e, tree) {
       var css = tree.toCSS({ compress: true });
       fs.writeFileSync(process.env.WERCKER_ROOT + '/' + process.env.WERCKER_GIT_COMMIT + '/' + current_page + '.css', css);
     });
