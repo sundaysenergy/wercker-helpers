26c0aede18f4a7620d4ab742b1d5ec521daa3fe0
Tim Lupfer <tim@sundaysenergy.com>
2013-12-02 13:52:17 -0600

    Cleanup and commenting

--- compile_assets.js
+++ compile_assets.js
@@ -12,17 +12,14 @@ var doc = require(process.env.WERCKER_ROOT + '/collection.yml');
 // Process each key
 for (var i in doc) {
   (function(current_page) {
-    var parser = new(less.Parser)({
-      paths: [process.env.WERCKER_ROOT]
-    });
+    var parser = new(less.Parser)({ paths: [process.env.WERCKER_ROOT] });
     var files = [];
-    var jsfiles = [];
-    // Push each file into an array as a string
+    // Push each .css file into an array as a string
     for (j in doc[current_page].css) {
       files.push(fs.readFileSync(process.env.WERCKER_ROOT + '/' + doc[current_page].css[j]).toString());
     }
+    // If we have .js files, compress and create a file
     if (doc[current_page].js.length > 0) {
-      console.log(doc[current_page].js);
       var compressedjs = uglifyjs.minify(doc[current_page].js);
       fs.writeFileSync(process.env.WERCKER_ROOT + '/' + process.env.WERCKER_GIT_COMMIT + '/' + current_page + '.js', compressedjs.code);
     }
