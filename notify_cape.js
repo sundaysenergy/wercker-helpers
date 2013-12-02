11cf56abde69b9f952691acfccb9543b8015140f
Tim Lupfer <tim@sundaysenergy.com>
2013-12-02 14:02:48 -0600

    Change notification url

--- notify_cape.js
+++ notify_cape.js
@@ -1,8 +1,8 @@
 /*** Wercker helper to notify cape that we should recompile the site ***/
 var http = require('http');
 
-http.get("http://v2.cape.io/" + process.env.CAPE_MAKEID + "/_view/*/process.json", function(res) {
-  console.log("Got response: " + res.statusCode);
+http.get("http://v2.cape.io/" + process.env.CAPE_MAKEID + "/_view/_all/process.json", function(res) {
+  console.log("Notifying " + "http://v2.cape.io/" + process.env.CAPE_MAKEID + "/_view/_all/process.json" + ": " + res.statusCode);
 }).on('error', function(e) {
   console.log("Got error: " + e.message);
 });
