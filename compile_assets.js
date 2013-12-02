9bee8839b3c29c6d884558df2d3bd0bcdcdaa8a9
Tim Lupfer <tim@sundaysenergy.com>
2013-12-02 13:08:45 -0600

    Only try to minify js if there is js to minify

--- compile_assets.js
+++ compile_assets.js
@@ -21,8 +21,11 @@ for (var i in doc) {
     for (j in doc[current_page].css) {
       files.push(fs.readFileSync(process.env.WERCKER_ROOT + '/' + doc[current_page].css[j]).toString());
     }
-    var compressedjs = uglifyjs.minify(doc[current_page].js);
-    fs.writeFileSync(process.env.WERCKER_ROOT + '/' + process.env.WERCKER_GIT_COMMIT + '/' + current_page + '.js', compressedjs);
+    if (doc[current_page].js.length > 0) {
+      console.log(doc[current_page].js);
+      var compressedjs = uglifyjs.minify(doc[current_page].js);
+      fs.writeFileSync(process.env.WERCKER_ROOT + '/' + process.env.WERCKER_GIT_COMMIT + '/' + current_page + '.js', compressedjs);
+    }
     // Parse the less and write a css file
     parser.parse(files.join(" "), function (e, tree) {
       var css = tree.toCSS({ compress: true });
